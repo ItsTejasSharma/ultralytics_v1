00d8da6ff1b23cd15fbbfba0b3be0930d9529042
ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>
2025-03-01 18:45:21 +0530

    Update head.py
    Signed-off-by: ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -537,9 +537,11 @@ def _get_encoder_input(self, x):
         # [b, h*w, c]
         feats = torch.cat(feats, 1)
         return feats, shapes
-
+    
     def _get_decoder_input(self, feats, shapes, dn_embed=None, dn_bbox=None):
+        
         """Generates and prepares the input required for the decoder from the provided features and shapes."""
+        
         bs = feats.shape[0]
         # Prepare input for decoder
         anchors, valid_mask = self._generate_anchors(shapes, dtype=feats.dtype, device=feats.device)
